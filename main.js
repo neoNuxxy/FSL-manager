a2044a19d1ff1082fd5a43e2fd7599b1150c59b5
Nic0laZim0lo <nicola@zimolo.eu>
2026-05-07 10:40:30 +0200

    cinzia

---
+++ main.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+cinzia
+=======
+<html>
+    <head>
+        <title>
+            This is a test
+        </title>
+    </head>
+
+    <body>
+        test
+    </body>
+</html>
+>>>>>>> 3a34923f232cc8406936f331613df642cb1b7b47
